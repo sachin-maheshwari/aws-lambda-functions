dc2e7f103fc7ff818afefea25f2141d876ae98da
Sachin Maheshwari <sachin.maheshwari@topcoder.com>
2018-11-23 18:58:40 +0530

    changes in test script

--- m2m-proxy/test.js
+++ m2m-proxy/test.js
@@ -8,7 +8,7 @@ let event = {
     "client_secret": process.env.AUTH0_CLIENT_SECRET || '',
     "audience": process.env.AUTH0_AUDIENCE || '',
     "auth0_url": process.env.AUTH0_URL || '',
-    "token_cache_time ": process.env.TOKEN_CACHE_TIME || 864000 // 24Hrs
+    "token_cache_time ": process.env.DEFAULT_TOKEN_CACHE_TIME || 864000 // 24Hrs
   }
 }
 lambda.handler(event, {}, function (callback, result) {
