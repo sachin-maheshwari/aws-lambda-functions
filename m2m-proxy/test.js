b7b54b07ab72418f0b472679f5f66495965a9ee8
Sachin Maheshwari <sachin.maheshwari@topcoder.com>
2018-11-23 18:51:03 +0530

    changes in test script

--- m2m-proxy/test.js
+++ m2m-proxy/test.js
@@ -1,14 +1,16 @@
 const lambda = require('./lambda')
 
-
-let event = {body : {
+// Authentication server setting 
+let event = {
+  body: {
     "grant_type": "client_credentials",
-    "client_id": "",
-    "client_secret": "",
-    "audience": "",
-    "auth0_url" : "",
-    "token_cache_time " : 60 
-  }}
-lambda.handler(event,{}, function(a,b) {
-    console.log(b)
+    "client_id": process.env.AUTH0_CLIENT_ID || '',
+    "client_secret": process.env.AUTH0_CLIENT_SECRET || '',
+    "audience": process.env.AUTH0_AUDIENCE || '',
+    "auth0_url": process.env.AUTH0_URL || '',
+    "token_cache_time ": process.env.TOKEN_CACHE_TIME || 864000 // 24Hrs
+  }
+}
+lambda.handler(event, {}, function (callback, result) {
+  console.log(result)
 })
